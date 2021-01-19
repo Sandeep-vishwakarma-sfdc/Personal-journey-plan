ac5f44d14a37a70dc20c6ecc1a84bb7c214fe1e3
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-01-19 13:08:40 +0530

    init commit

---
+++ .sfdx/typings/lwc/apex/ApprovalComponentController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ApprovalComponentController.getApprovalData" {
+  export default function getApprovalData(param: {recId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ApprovalComponentController.getProcessHistory" {
+  export default function getProcessHistory(param: {objectId: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/PjpService.d.ts
@@ -0,0 +1,108 @@
+declare module "@salesforce/apex/PjpService.getAccountList" {
+  export default function getAccountList(param: {filter1: any, filter2: any, filter3: any, subclass: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getArea" {
+  export default function getArea(param: {city_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getUser" {
+  export default function getUser(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getUserforGM" {
+  export default function getUserforGM(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getCurrentUser" {
+  export default function getCurrentUser(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getCity" {
+  export default function getCity(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getAreaAll" {
+  export default function getAreaAll(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.saveAsDraft" {
+  export default function saveAsDraft(param: {eventToCreate: any, usr_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.submitForApproval" {
+  export default function submitForApproval(param: {pjp_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getFilterList1" {
+  export default function getFilterList1(param: {currentMonth: any, startDate: any, endDate: any, usr_id: any, profilename: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getProfile" {
+  export default function getProfile(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getUserid" {
+  export default function getUserid(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getUsername" {
+  export default function getUsername(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getSubzone" {
+  export default function getSubzone(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getcityOnArea" {
+  export default function getcityOnArea(param: {area_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getMonth" {
+  export default function getMonth(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getMOnthbyId" {
+  export default function getMOnthbyId(param: {mon_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getMonthOnUser" {
+  export default function getMonthOnUser(param: {usr_id: any, mon_name: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getMonthdetails" {
+  export default function getMonthdetails(param: {month_value: any, usr_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getMonthmap" {
+  export default function getMonthmap(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.deleteDetails" {
+  export default function deleteDetails(param: {lst: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getBdmuser" {
+  export default function getBdmuser(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getRkamUser" {
+  export default function getRkamUser(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getNkamUser" {
+  export default function getNkamUser(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getZone" {
+  export default function getZone(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.rkmCity" {
+  export default function rkmCity(param: {zone: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.rkmArea" {
+  export default function rkmArea(param: {zone: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getpjp" {
+  export default function getpjp(param: {usr_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getAccountsubclass" {
+  export default function getAccountsubclass(param: {str: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getUserbyId" {
+  export default function getUserbyId(param: {usr_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.approvePjp" {
+  export default function approvePjp(param: {pjp_id: any, comment: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.rejectPjp" {
+  export default function rejectPjp(param: {pjp_id: any, comment: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getSubclassFromApex" {
+  export default function getSubclassFromApex(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getpjpUserId" {
+  export default function getpjpUserId(param: {pjp_id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getCityforGM" {
+  export default function getCityforGM(): Promise<any>;
+}
+declare module "@salesforce/apex/PjpService.getAreaforGM" {
+  export default function getAreaforGM(): Promise<any>;
+}
